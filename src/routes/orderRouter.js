c1eaed75968b83fe104bd5df5db92687b0d741c0
Josh <josh.a.wiseman@gmail.com>
2025-01-16 14:38:11 -0700

    order router needed a patch bc old node.js

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -6,6 +6,11 @@ const { asyncHandler, StatusCodeError } = require('../endpointHelper.js');
 
 const orderRouter = express.Router();
 
+let fetch;
+(async () => {
+  fetch = (await import('node-fetch')).default;
+})();
+
 orderRouter.endpoints = [
   {
     method: 'GET',
